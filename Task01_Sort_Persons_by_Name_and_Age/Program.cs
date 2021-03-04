569e863e3660d95c13506c3d32f3d81ca34bdf4e
Kiril Kirilov <kirilkirilov@abv.bg>
2021-03-04 09:32:23 +0200

    Add files via upload

---
+++ Task01_Sort_Persons_by_Name_and_Age/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int linesNumber = int.Parse(Console.ReadLine());
+            List<Person> names = new List<Person>();
+
+            for (int i = 1; i <= linesNumber; i++)
+            {
+                string[] nextLineData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                Person nextName = new Person(nextLineData[0], nextLineData[1], int.Parse(nextLineData[2]));
+
+                names.Add(nextName);
+
+            }
+
+            names.OrderBy(p => p.FirstName).ThenBy(n => n.Age).ToList().ForEach(n => Console.WriteLine(n.ToString()));
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
