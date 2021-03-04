770dfc77ae8e3980579984b60f7d67fb829e8901
Kiril Kirilov <kirilkirilov@abv.bg>
2021-03-04 09:34:11 +0200

    Add files via upload

---
+++ Task03_Validation_of_Data/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int linesNumbers = int.Parse(Console.ReadLine());
+            List<Person> names = new List<Person>();
+
+            try
+            {
+                for (int i = 1; i <= linesNumbers; i++)
+                {
+                    string[] nextLineDatas = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                    Person nextName = new Person(nextLineDatas[0], nextLineDatas[1], int.Parse(nextLineDatas[2]), decimal.Parse(nextLineDatas[3]));
+                    names.Add(nextName);
+                }
+
+                int percentage = int.Parse(Console.ReadLine());
+
+                foreach (Person name in names)
+                {
+                    name.IncreaseSalary(percentage);
+                    Console.WriteLine(name);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            
+            
+
+            
+        }
+    }
+}
